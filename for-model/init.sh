4b6978a9b1c1e882c8e3694b4bee24256824be6f
kyusoo <rbtn110@uengine.org>
2024-06-12 14:27:20 +0900

    init.sh로 java17버전 설치

--- for-model/init.sh
+++ for-model/init.sh
@@ -6,6 +6,9 @@ pip install httpie
 sudo apt update
 sudo apt install openjdk-17-jdk
 
+yes | sdk install java 17.0.7-open
+
+
 export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
 
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
